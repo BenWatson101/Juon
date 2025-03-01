4598eac5d2d66a147a90178cdb669f93b69a19bb
zzr202 <97767089+zzr202@users.noreply.github.com>
2025-03-01 15:06:45 -0500

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            char e = (char)UniversalObject.parse(UniversalObject.convert('e').json());
+            char f = (char)UniversalObject.parse(UniversalObject.convert('e').json());
 
-            System.out.println(e);
+            System.out.println(f);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
